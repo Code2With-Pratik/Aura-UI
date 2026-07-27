7e8e126869f4059b319cbf696110bd1947722fd0
Code2With-Pratik <work.pratik1204@gmail.com>
2026-07-27 07:02:30 +0000

    added the vercel analytics to check the number of users online

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,13 +1,25 @@
 import type { ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/next";
+
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-export default function SiteLayout({ children }: { children: ReactNode }) {
+export default function SiteLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <>
       <Navbar />
-      <div className="pt-[68px]">{children}</div>
+
+      <div className="pt-[68px]">
+        {children}
+      </div>
+
       <Footer />
+
+      <Analytics />
     </>
   );
 }
