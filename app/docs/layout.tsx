803a9ec6570d5542d175b686d64cae17a362a52d
Code2With-Pratik <work.pratik1204@gmail.com>
2026-09-02 11:46:42 +0000

    removed the dropdown

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -3,7 +3,6 @@ import type { ReactNode } from "react";
 import { source } from "@/lib/source";
 import { baseOptions } from "../layout.config";
 import Navbar from "@/components/Navbar";
-import DocsSidebarItem from "./DocsSidebarItem";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
@@ -14,7 +13,6 @@ export default function Layout({ children }: { children: ReactNode }) {
           tree={source.pageTree}
           {...baseOptions}
           nav={{ ...baseOptions.nav, enabled: false }}
-          sidebar={{ components: { Item: DocsSidebarItem } }}
         >
           {children}
         </DocsLayout>
